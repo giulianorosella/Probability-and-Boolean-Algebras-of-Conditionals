ab2f15f29866b537b234b4e72015efe0b3413be4
giulianorosella <giulianoros96@gmail.com>
2022-03-17 00:00:31 +0100

    belief of all

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ from inputs import all_basic_conditionals
 
 
 
-bc_in_question = (list(input('I want to find the atoms below and the probability of the basic conditional whose consequent is: ')), list(input('and whose antecedent is: ')))
+#bc_in_question = (list(input('I want to find the atoms below and the probability of the basic conditional whose consequent is: ')), list(input('and whose antecedent is: ')))
 
-print(bc_in_question)
+#print(bc_in_question)
 
-print('The atoms below are: ')
-print(atoms_below(bc_in_question))
+#print('The atoms below are: ')
+#print(atoms_below(bc_in_question))
 
-print('Its Box is: ')
-print(box(bc_in_question))
+#print('Its Box is: ')
+#print(box(bc_in_question))
 
-prob_below = []
-for i in atoms_below(bc_in_question):
-    prob_below.append(list_prob(i))
+#prob_below = []
+#for i in atoms_below(bc_in_question):
+#    prob_below.append(list_prob(i))
 
-print('Its probability is: ')
-print(sum(prob_below))
+#print('Its probability is: ')
+#print(sum(prob_below))
 
-print('Its belief is: ')
-print(belief(all_basic_conditionals[2]))
+#print('Its belief is: ')
+#print(belief(all_basic_conditionals[2]))
 
 
 
@@ -35,6 +35,15 @@ for i in all_basic_conditionals:
     counterfactuals.append(box(i))
     print('The box of ' +str(i)+ ' is: ')
     print(box(i))
+    at_below = []
+    for j in atoms_below(i):
+        at_below.append(list_prob(j))
+    print('The probability of ' +str(i)+ ' is: ')
+    print(sum(at_below))
+    print('The belief of ' +str(i)+ ' is: ')
+    print(belief(i))
+
+
 
 
 
